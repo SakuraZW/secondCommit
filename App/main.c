640b8e4bf51132b6fa88a4ef93da8c8b0748c5b6
sakura_glb <sakura@whut.com>
2020-01-01 09:24:14 +0800

    modify main.c

--- App/main.c
+++ App/main.c
@@ -4,6 +4,12 @@ int main()
 {
   ftm_pwm_init(FTM0, FTM_CH0, 20000, 200);
 	  ftm_pwm_init(FTM0, FTM_CH1, 20000, 0);
+          while(1);
+          while(1);
+          while(1);
+          while(1);
+          while(1);
+          while(1);
 	
 while(1);
 }
